b80e9af604af4ab8143cc25617cfb400552e3fd0
mcatalan15 <marc.prat.2001@gmail.com>
2023-11-05 21:09:24 +0100

    cleaning include

--- src/so_long.h
+++ src/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 15:45:47 by mcatalan          #+#    #+#             */
-/*   Updated: 2023/11/03 11:48:12 by mcatalan         ###   ########.fr       */
+/*   Updated: 2023/11/05 21:08:54 by mcatalan@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 # include "../includes/libft/libft.h"
 # include "../includes/mlx/mlx.h"
 # include "graphic.h"
-// # include "get_next_line.h"
 
 # include <fcntl.h>
 # include <unistd.h>
@@ -32,50 +31,50 @@
 # define SIZE 48
 # define MAP_CHARS "10PCE\n"
 
-t_block	set_block(char name);
-
-int		key_check(int key_code, t_game *game);
+// window.c
 int		destroy_window(t_game *game);
-int		ft_line_length(int fd);
-int		ft_count_lines(int fd);
-
-void	baslangic(t_game *game);
-void	message(char *msg, t_game *game);
-void	wall_control(t_game *game);
-void	map_control(t_game *game);
-void	game_control(t_game *game);
-int		move_check(t_game *game, int i, int j);
 void	ft_window_size(t_game *data, char **argv);
-void	*myfree(void *str);
-void	*put_image(t_image image, int x, int y);
-void	create_map(t_game *game, int i);
-void	create_floor(t_game game, int b);
-void	create_map_length(char *area, t_game *game, int i);
-void	create_map_line(t_game *game, char **argv);
+
+// movements.c
 void	update(t_game *game, int x, int y);
 void	right(t_game *game);
 void	up(t_game *game);
 void	down(t_game *game);
 void	left(t_game *game);
+
+// map.c
 int		map_destroy(t_game *game);
+void	create_map(t_game *game, int i);
+void	create_floor(t_game game, int b);
+void	create_map_length(char *area, t_game *game, int i);
+void	create_map_line(t_game *game, char **argv);
 
-char	*get_next_line(int fd);
-void	messages(char *msg, t_game *game);
-
-void	checker(t_game *game);
+// controlls.c
+void	wall_control(t_game *game);
+void	map_control(t_game *game);
+void	game_control(t_game *game);
+int		move_check(t_game *game, int i, int j);
 
 //map2.c
-// void create_map_line_copy(t_game *game, char **argv);
-// void map_destroy_copy(t_game *game);
-
 void	print_map(t_game *game);
 void	print_map_copy(t_game *game);
 
 // utils2.c
-void	print_map(t_game *game);
-void	print_map_copy(t_game *game);
-
 int		msg(char *str);
+void	message(char *msg, t_game *game);
+void	print_map_copy(t_game *game);
+void	print_map(t_game *game);
+t_block	set_block(char name);
+
+// utils.c
+void	*put_image(t_image image, int x, int y);
+int		ft_line_length(int fd);
+int		ft_count_lines(int fd);
+void	*myfree(void *str);
+char	*get_next_line(int fd);
+
+// checker.c
+void	checker(t_game *game);
 
 // checker2.c
 int		copy_map(t_game *game);
